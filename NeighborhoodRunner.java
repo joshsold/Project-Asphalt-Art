28a3dc364589dff6f83b3bcde2f6adb8b49bea2b
joshsold <joshuataehwanlee@gmail.com>
2024-09-30 08:35:54 -0700

    Update NeighborhoodRunner.java

--- NeighborhoodRunner.java
+++ NeighborhoodRunner.java
@@ -1,1 +1,15 @@
 
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+    
+    MuralPainter mp = new MuralPainter();
+    DawgPainter dp = new DawgPainter();
+    
+    mp.paintBackground("darkSeaGreen", 32);
+    dp.paintBeauty();
+    
+  }
+}
